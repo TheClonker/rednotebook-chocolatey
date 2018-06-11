19c0f5dba434d2018dc42fde34c7bae82adba983
Philip Gisella <philip.gisella@googlemail.com>
2018-06-11 05:35:23 +0200

    Upgrade to Version 2.5

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,13 +1,13 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://github.com/jendrikseipp/rednotebook/releases/download/v2.4/rednotebook-2.4.exe'
+$url        = 'https://github.com/jendrikseipp/rednotebook/releases/download/v2.5/rednotebook-2.5.exe'
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
   fileType      = 'exe'
   url           = $url
   softwareName  = 'RedNotebook*'
-  checksum      = '0190011BFB8DE2E0ADA0B43C14B6799CC572E0910AFEA55EF02C95EE448A0833'
+  checksum      = '7A1FD65515B38BE86022BFB13E9EB72B65BDBFA4B7349C5DA7F7AFDCD5D835B5'
   checksumType  = 'sha256'
   silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-' # Inno Setup
   validExitCodes= @(0)
