2a70eacf6218dffe34f9aeb7df1f6c79d749099b
Philip Gisella <philip.gisella@googlemail.com>
2019-01-04 21:41:42 +0100

    Prep for automated Deployment

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,13 +1,13 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://github.com/jendrikseipp/rednotebook/releases/download/v2.8/rednotebook-2.8.exe'
+$url        = '%%URL%%'
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
   fileType      = 'exe'
   url           = $url
   softwareName  = 'RedNotebook*'
-  checksum      = 'C7927D9939CFE18C1CEE9CE48B884545AC3338A79CC718C3F8EF24FBB32CC3C3'
+  checksum      = '%%CHECKSUM%%'
   checksumType  = 'sha256'
   silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-' # Inno Setup
   validExitCodes= @(0)
